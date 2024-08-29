cd017ceb599455d7f12d917c7196ecdf3f41359b
Camilli.M <110904867+erica20072819@users.noreply.github.com>
2024-08-29 08:53:36 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,121 @@
+export const perguntas = [
+    {
+        enunciado: "Assim que saiu de sua casa viu seu primeiro desafio, dois caminhos, uma linda floresta e o outro caminho que a levava a atravessar a cidade. Indo pela floresta ela cortaria caminho mas pela cidade era mais seguro. ", //0
+        alternativas: [
+            {
+                texto: "Ir pela cidade",
+                afirmacao: [
+                    "Graças ah ela ter seguido as dicas de sua maẽ e ir pela cidade ela chegou em segurança."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Ir pela floresta",
+                afirmacao: [
+                    "Ela ter ido pela floresta não foi uma boa decisão, pois la havia muitos perigos."
+
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+    {
+        enunciado: "Assim que começou a andar viu que o caminho era longo e pensou em descansar, mas se ela parar agora irá chegar tarde.",
+        alternativas: [
+            {
+                texto: "Parar para descansar e chegar tarde.", //1
+                afirmacao: [
+                    "Por ela ter parado acabou chegando muito tarde."
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Continuar e se sentir muito cansada.",
+                afirmacao: [
+                    "Ainda que bem cansada não parou e chegou cedo e bem."
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Assim que começou a andar viu um campo de flores lindas, ela pensou em pegar algumas flores para sua vovó, mas sua mãe disse que não era para fazer pausas e ir direto para casa de sua vó. ", //2
+        alternativas: [
+            {
+                texto: "Parar e pegar lindas flores para sua vó.",
+                afirmacao: [
+                    "Ela chegou com um lindo buquê e alegrou sua vó, mas acabou chegando tarde."
+                    
+                ],
+                proxima: 3,
+            },
+            {
+                texto: "Continuar e obedecer sua mãe.",
+                afirmacao: [
+                    "chegou mais cedo a casa de sua avó."
+                    
+                ],
+                proxima: 4,
+            },
+        ]
+    },
+    {
+        enunciado: "Logo ela viu que tinha feito uma escolha errada pois já estava escurecendo e ia chegar muito tarde na casa de vó, pensou se continuava ou se retornava.  ",
+        alternativas: [
+            {
+                texto: "Continuar.",
+                afirmacao: [
+                    "Ela chegou muito tarde na casa de sua vó mas completou a missão que lhe foi dada."
+                ],
+                proxima: 5,
+            },
+            {
+                texto: "Retornar.",
+                afirmacao: [
+                    "Ela voltou para sua casa mas não teve problemas pois sua mãe ah compreendeu."
+                ],
+                proxima: 6,
+            },
+        ]
+    },
+    {
+        enunciado: "Logo ela chegou sem muitos problemas na casa de sua vovó",
+        alternativas: [
+            {
+                texto: "finalizar.",
+                afirmacao: [
+                    "E assim acabou o grande dia de Chapeuzinho."
+                ],
+                
+            }
+
+        ]
+    },
+    {
+        enunciado: "Ela acabou chegando muito tarde na casa de sua vó, mas conseguiu chegar no seu destino.",
+        alternativas: [
+            {
+                texto: "finalizar.",
+                afirmacao: [
+                    "E assim acabou o grande dia de Chapeuzinho."
+                ],
+                
+            }
+            
+        ]
+    },
+    {
+        enunciado: "Logo que chegou Chapeuzinho contou oque havia acontecido. Sua mãe então disse que estva tudo bem que no dia seguinte as duas iriam juntas até a casa da vovó. ",
+        alternativas: [
+            {
+                texto: "finalizar.",
+                afirmacao: [
+                    "E assim acabou o grande dia de Chapeuzinho."
+                ],
+                
+            }
+           
+        ]
+    },
+    
+];
